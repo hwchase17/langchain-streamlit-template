442e35a01a1c87f58860dd509b73bfe9d07b853d
jacoblee93 <jacoblee93@gmail.com>
2025-01-10 18:34:24 -0800

    Update with LangGraph

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # LangChain-Streamlit Template
 
-This repo serves as a template for how to deploy a LangChain on Streamlit.
+This repo serves as a template for how to deploy a [LangGraph](https://langchain-ai.github.io/langgraph/) agent on Streamlit.
 
 This repo contains an `main.py` file which has a template for a chatbot implementation.
 
 ## Adding your chain
+
 To add your chain, you need to change the `load_chain` function in `main.py`.
 Depending on the type of your chain, you may also need to change the inputs/outputs that occur later on.
 
 ## Deploy on Streamlit
 
 This is easily deployable on the Streamlit platform.
-Note that when setting up your StreamLit app you should make sure to add `OPENAI_API_KEY` as a secret environment variable.
+Note that when setting up your Streamlit app you should make sure to add `OPENAI_API_KEY` as a secret environment variable.
+
+## Setting up LangSmith
+
+To quickly spot issues and improve the performance of your LangGraph projects, sign up for [LangSmith](https://docs.smith.langchain.com/). LangSmith lets you use trace data to debug, test, and monitor your LLM apps built with LangGraph — read more about how to get started [here](https://docs.smith.langchain.com/).
--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 """Python file to serve as the frontend"""
+import uuid
+
 import streamlit as st
 from streamlit_chat import message
 
-from langchain.chains import ConversationChain
-from langchain.llms import OpenAI
+from langgraph.prebuilt import create_react_agent
+from langchain.chat_models import init_chat_model
+from langgraph.checkpoint.memory import MemorySaver
 
-
-def load_chain():
-    """Logic for loading the chain you want to use should go here."""
-    llm = OpenAI(temperature=0)
-    chain = ConversationChain(llm=llm)
-    return chain
-
-chain = load_chain()
+def load_agent():
+    """Logic for loading the agent you want to use should go here."""
+    llm = init_chat_model("gpt-4o-mini", model_provider="openai", temperature=0)
+    tools = []
+    prompt = "Always respond like a pirate."
+    checkpointer = MemorySaver()
+    # https://langchain-ai.github.io/langgraph/reference/prebuilt/#langgraph.prebuilt.chat_agent_executor.create_react_agent
+    graph = create_react_agent(
+        llm,
+        tools=tools,
+        state_modifier=prompt,
+        checkpointer=checkpointer
+    )
+    return graph
 
 # From here down is all the StreamLit UI.
 st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
@@ -24,6 +33,12 @@ if "generated" not in st.session_state:
 if "past" not in st.session_state:
     st.session_state["past"] = []
 
+# Add thread_id and checkpointer initialization
+if "thread_id" not in st.session_state:
+    st.session_state["thread_id"] = str(uuid.uuid4())
+
+if "agent" not in st.session_state:
+    st.session_state["agent"] = load_agent()
 
 def get_text():
     input_text = st.text_input("You: ", "Hello, how are you?", key="input")
@@ -33,10 +48,15 @@ def get_text():
 user_input = get_text()
 
 if user_input:
-    output = chain.run(input=user_input)
+    output = st.session_state["agent"].invoke({
+        "messages": [{"role": "user", "content": user_input}]
+    }, {
+        "configurable": {"thread_id": st.session_state.thread_id}
+    })
 
     st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
+    # Final state in the messages are the output
+    st.session_state.generated.append(output["messages"][-1].content)
 
 if st.session_state["generated"]:
 
